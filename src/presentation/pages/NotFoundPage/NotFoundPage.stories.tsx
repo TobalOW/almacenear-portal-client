8956daa6f7396b0b3a12522b7b325729bf28199a
cristobal.miranda.u@gmail.com <TobalOW>
2020-08-07 18:25:28 -0400

    feat(http): add not found page

---
+++ src/presentation/pages/NotFoundPage/NotFoundPage.stories.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { NotFoundPage } from "./index";
+
+export default { title: "Pages/NotFound" };
+
+export const NotFound = () => <NotFoundPage />;
---
+++ src/presentation/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex, Heading } from "@chakra-ui/core";
+
+const NotFoundPage = () => {
+  return (
+    <Flex width="full" justifyContent="center" align="center">
+      <Heading>Pagina no encontrada</Heading>
+    </Flex>
+  );
+};
+
+export default NotFoundPage;
