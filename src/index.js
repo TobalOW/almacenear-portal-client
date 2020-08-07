f0c54a574e7fecd4eec992e63b11d913a43b40cf
cristobal.miranda.u@gmail.com <TobalOW>
2020-08-07 18:23:47 -0400

    feat(core): add Redux with HRM

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import store from "./redux/store";
+import store from "./domain/redux/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import theme from "./presentation/theme";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CSSReset />
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+render();
+
+if (process.env.NODE_ENV === "development" && module.hot) {
+  module.hot.accept("./App.tsx", render);
+}
 serviceWorker.unregister();
