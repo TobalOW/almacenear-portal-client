eaac4e78b8c48d31bc63dda6fe6bcb745a7d5f13
cristobal.miranda.u@gmail.com <TobalOW>
2020-08-07 18:26:00 -0400

    feat(http): add loginform

--- src/presentation/organisms/LoginForm/LoginForm.tsx
+++ src/presentation/organisms/LoginForm/LoginForm.tsx
@@ -1,25 +1,67 @@
 import React from "react";
 import {
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
+  Box,
+  Text,
+  Heading,
+  Link,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Checkbox,
+  Button,
 } from "@chakra-ui/core";
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 
 const LoginForm = () => {
   return (
-    <ThemeProvider>
-      <CSSReset />
-      <Alert status="error">
-        <AlertIcon />
-        <AlertTitle mr={2}>Your browser is outdated!</AlertTitle>
-        <AlertDescription>
-          Your Chakra experience may be degraded.
-        </AlertDescription>
-        {/* <CloseButton position="absolute" right="8px" top="8px" /> */}
-      </Alert>
-    </ThemeProvider>
+    <Box
+      borderWidth={1}
+      px={4}
+      width="full"
+      maxWidth="500px"
+      borderRadius={4}
+      textAlign="center"
+      boxShadow="lg"
+      backgroundColor={"white"}
+    >
+      <Box p={4}>
+        <Box textAlign="center">
+          <Text>Portal Administrativo</Text>
+          <Heading>
+            <Text wordBreak={"break-word"} color={"red.500"}>
+              AlmaceNear
+            </Text>
+          </Heading>
+        </Box>
+
+        <Box my={8} textAlign="left">
+          <form>
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" placeholder="Ingresa tu email" />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Contrasena</FormLabel>
+              <Input type="password" placeholder="Ingresa tu contrasena" />
+            </FormControl>
+
+            <Stack isInline justifyContent="space-between" mt={4}>
+              <Box>
+                <Checkbox>Recuerdame!</Checkbox>
+              </Box>
+              <Box>
+                <Link color={`red.500`}>¿Olvidaste tu contasena?</Link>
+              </Box>
+            </Stack>
+
+            <Button width="full" mt={4} color={"red.500"}>
+              Ingresar
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
