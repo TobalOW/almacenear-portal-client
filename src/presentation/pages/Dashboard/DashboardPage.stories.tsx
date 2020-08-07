902a6e1dce1e40b51ffb5727615fd546fd899213
cristobal.miranda.u@gmail.com <TobalOW>
2020-08-07 18:24:22 -0400

    feat(http): add dashboard page

---
+++ src/presentation/pages/Dashboard/DashboardPage.stories.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { DashboardPage } from ".";
+
+export default { title: "Pages/Dashboard" };
+
+export const Dashboard = () => <DashboardPage />;
---
+++ src/presentation/pages/Dashboard/DashboardPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Flex } from "@chakra-ui/core";
+import { Navbar } from "../../organisms/Navbar";
+import { SideMenu } from "../../organisms/SideMenu";
+
+const DashboardPage = () => {
+  return (
+    <Flex height="100%" width="100%" flexDirection="column">
+      <Navbar />
+      <SideMenu />
+    </Flex>
+  );
+};
+export default DashboardPage;
