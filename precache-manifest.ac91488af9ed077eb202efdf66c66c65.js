7cdb5da63bd589ca6f7b8a6c13e0f3e23cf63363
Alejandro L <alejandrolopez.gd@gmail.com>
2020-10-03 20:12:18 -0300

    Updates

---
+++ precache-manifest.ac91488af9ed077eb202efdf66c66c65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7cf93c5f94bfdd00c91cc74702a0aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc1e7b90d71af343b577",
+    "url": "/static/js/2.8782b136.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8782b136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08242dfd2839a6e78867",
+    "url": "/static/js/main.bb5b8029.chunk.js"
+  },
+  {
+    "revision": "6a530fbb475e6637cd73",
+    "url": "/static/js/runtime-main.9fcf3071.js"
+  }
+]);
