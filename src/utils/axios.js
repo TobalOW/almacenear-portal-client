5e629ca3fecd260ac31b407fc6e40b2d3955bece
Alejandro L <alejandrolopez.gd@gmail.com>
2020-10-25 18:07:55 -0300

    fix(api): change to HTTPS

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://demo5731898.mockable.io/",
+  baseURL: "https://demo5731898.mockable.io/",
 });
 
 export default axiosInstance;
